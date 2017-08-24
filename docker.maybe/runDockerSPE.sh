e53a23ac1d1261c6dec1df8268d93bec13182b3c
David Haines <dlhaines@umich.edu>
2017-08-24 16:34:20 -0400

    TLSPE-7: checkpoint Docker for OS

--- docker.maybe/runDockerSPE.sh
+++ docker.maybe/runDockerSPE.sh
@@ -1,13 +1,29 @@
-# Run spanish placement exam script
+# Run Spanish placement exam script with a specific tag after image is built.
 #################
 
 set -x
 echo "run SPE in docker"
 
-# test that can specify args on startup
-ARGS=" --spring.profiles.include=ZOMBIE "
+TAG=spe_a
 
-docker run  -e TZ=America/New_York  spe_a ${ARGS}
+ENVIRONMENT=" -e TZ=America/New_York "
+
+HOST_PERSIST=${PWD}/tmp/persist
+CONTAINER_PERSIST=/tmp/persist_string
+V_PERSIST=" -v ${HOST_PERSIST}:${CONTAINER_PERSIST} "
+
+HOST_PUT_GRADE=${PWD}/tmp/put_grade
+CONTAINER_PUT_GRADE=/opt/spe/files
+V_PUT_GRADE=" -v ${HOST_PUT_GRADE}:${CONTAINER_PUT_GRADE} "
+
+mkdir -p ${HOST_LOG}
+mkdir -p ${HOST_PERSIST}
+
+# Arguments to pass to Spring Boot.
+#SP_ARGS=" --spring.profiles.include=INTEGRATION "
+
+echo "Running docker image with tag [${TAG}] with environment [${ENVIRONMENT}] and extra args: [${SP_ARGS}]"
+docker run -it ${ENVIRONMENT} ${V_PERSIST} ${V_PUT_GRADE} --rm ${TAG} ${SP_ARGS}
 
 #end
 
