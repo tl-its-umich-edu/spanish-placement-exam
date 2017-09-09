861a5580092867f5fe8f7958dd9fb581a196dbb5
David Haines <dlhaines@umich.edu>
2017-09-09 17:01:59 -0400

    TLSPE-9: checkpoint

--- bin/addTag.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#oc tag 172.30.183.221:5000/spanish-placement-exam-dev/spanish-placement-exam spanish-placement-exam:spedevA
-
-SOURCE=172.30.183.221:5000/spanish-placement-exam-dev/spanish-placement-exam
-DEST=spanish-placement-exam
-TAG=spedevA
-set -x
-oc tag ${SOURCE} ${DEST}:${TAG}
-#end
--- bin/runDockerSPE.sh
+++ bin/runDockerSPE.sh
@@ -12,18 +12,18 @@ TAG=spe_a
 ENVIRONMENT=" -e TZ=America/New_York "
 
 ######
-HOST_PERSIST=$(pwd)/docker/tmp/persist
+HOST_PERSIST=$(pwd)/tmp/persist
 [ -e "${HOST_PERSIST}" ] || { echo "ERROR: [${HOST_PERSIST}] must exist" && exit 1; }
 #CONTAINER_PERSIST=/tmp/persist_string
 CONTAINER_PERSIST=/opt/spe_persist
 V_PERSIST=" -v ${HOST_PERSIST}:${CONTAINER_PERSIST} "
 ##
-HOST_PUT_GRADE=$(pwd)/docker/tmp/put_grade
+HOST_PUT_GRADE=$(pwd)/tmp/put_grade
 [ -e "${HOST_PUT_GRADE}" ] || { echo "ERROR: [${HOST_PUT_GRADE}] must exist" && exit 1; }
 CONTAINER_PUT_GRADE=/opt/spe/files
 V_PUT_GRADE=" -v ${HOST_PUT_GRADE}:${CONTAINER_PUT_GRADE} "
 ##
-#HOST_SECRETS=$(pwd)/docker/tmp/secrets
+#HOST_SECRETS=$(pwd)/bin/tmp/secrets
 HOST_SECRETS=$(pwd)/config
 [ -e "${HOST_SECRETS}" ] || { echo "ERROR: [${HOST_SECRETS}] must exist" && exit 1; }
 CONTAINER_SECRETS=/opt/secrets
@@ -38,7 +38,8 @@ mkdir -p ${HOST_PERSIST}
 # Arguments to pass to Spring Boot.
 # try multiple args in startup
 #SP_ARGS=" --spring.profiles.include=OS-dev,MASTERofNONE  --spring.config.location=classpath:/external/properties/,classpath:/com/roufid/tutorial/configuration/"
-SP_ARGS=" --spring.profiles.include=OS-dev,QAINTEGRATION  --spring.config.location=file:/opt/secrets/"
+#SP_ARGS=" --spring.profiles.include=OS-dev,QAINTEGRATION  --spring.config.location=file:/opt/secrets/"
+SP_ARGS=" --spring.profiles.include=QAINTEGRATION  --spring.config.location=file:/opt/secrets/"
 
 
 echo "Running docker image with tag [${TAG}] "
