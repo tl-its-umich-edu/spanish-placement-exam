4323f3622f08df9d614b1376634f9606197ef8af
David Haines <dlhaines@umich.edu>
2017-09-21 13:08:52 -0400

    TLSPE-34: clean up import

--- src/main/java/edu/umich/its/spe/SPEMaster.java
+++ src/main/java/edu/umich/its/spe/SPEMaster.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 import com.mashape.unirest.http.Unirest;
