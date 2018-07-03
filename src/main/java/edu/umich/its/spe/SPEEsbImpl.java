34fb80dc7549f126688b0b20f2727e8193a4dd4c
David Haines <dlhaines@umich.edu>
2018-07-03 16:37:36 -0400

    TLPSO-454: initial version to reuse WAPI connection."

--- src/main/java/edu/umich/its/spe/SPEEsbImpl.java
+++ src/main/java/edu/umich/its/spe/SPEEsbImpl.java
@@ -26,6 +26,7 @@ import edu.umich.ctools.esb.utils.WAPIResultWrapper;
 @Component
 @Qualifier("ESBIO")
 
+
 public class SPEEsbImpl implements GradeIO {
 
 	protected static final String SKIP_GRADE_UPDATE = "SKIP GRADE UPDATE";
@@ -35,17 +36,64 @@ public class SPEEsbImpl implements GradeIO {
 	@Autowired
 	SPESummary spesummary;
 
+	// Hold configuration for connecting to the ESB.
+	// If there are multiple WAPI connections required then will need multiple variables.
+	private WAPI wapi = null;
+
+	// Can set to always return a new connection.  Primarily for testing.
+	private Boolean reuseConnection = true;
+
 	// No arg constructor for use in SpringApp.
 	public SPEEsbImpl() {
 		super();
 	}
 
-	// Constructor that allows injecting summary object.  That can
-	// be useful in testing when don't want to require using Spring injection.
+
+	// Constructors that allows injecting objects.  This can be useful
+	// in testing when don't want to require using Spring injection.
+	// Only the necessary constructors has been created.  There is probably a
+	// better way to do this.  The fundamental issue was injecting mocks.
 
 	public SPEEsbImpl(SPESummary spesummary) {
 		super();
 		this.spesummary = spesummary;
+	}
+
+	public SPEEsbImpl(WAPI wapi, SPESummary spesummary) {
+		super();
+		this.wapi = wapi;
+		this.spesummary = spesummary;
+	}
+
+	// allows testing default value of reuseConnection.
+	public SPEEsbImpl(WAPI wapi) {
+		super();
+		this.wapi = wapi;
+	}
+
+	public SPEEsbImpl(Boolean reuseConnection) {
+		super();
+		this.reuseConnection = reuseConnection;
+	}
+
+	// Make sure there is a useful connector to the ESB.  Pass in the value of caller WAPI by reference
+	// so a) can check if it is already set and b) this could be used for multiple different connections if necessary.
+	// This can't currently be called at calling class creation since don't have the properties file available at
+	// that time. Each method using WAPI needs to call this.
+
+	public WAPI assureWAPI(WAPI wapi, HashMap<String,String> ioproperties) {
+
+		// This line allows testing to see if this change makes a difference in the number of calls to the ESB.
+		if (this.reuseConnection == false) {
+			wapi = null;
+		}
+
+		// Don't check here for problems, allow any problem to be dealt with in the call chain.
+		if (wapi == null) {
+			wapi = new WAPI(ioproperties);
+		}
+
+		return wapi;
 	}
 
 	/******************** Get grades from ESB (Unizin Data Warehouse) ******************/
@@ -82,15 +130,15 @@ public class SPEEsbImpl implements GradeIO {
 		String url = formatGetURLTemplate(values);
 		M_log.debug("getGrades: request url: [" + url.toString() + "]");
 
-		WAPI wapi = new WAPI(values);
+		this.wapi = assureWAPI(this.wapi,speproperties.getIo());
 
 		WAPIResultWrapper wrappedResult = wapi.doRequest(url.toString(),headers);
 
 		if (wrappedResult.getStatus() != HttpStatus.SC_OK && wrappedResult.getStatus() != HttpStatus.SC_NOT_FOUND) {
 			String msg = "error in esb call to get grades: status: "+wrappedResult.getStatus()+" message: "
-						+wrappedResult.getMessage()
-						+" URL: "+ url.toString()
-						+" Headers: "+headers;
+					+wrappedResult.getMessage()
+					+" URL: "+ url.toString()
+					+" Headers: "+headers;
 			M_log.error(msg,wrappedResult.toString());
 			throw(new GradeIOException(msg));
 		}
@@ -104,8 +152,9 @@ public class SPEEsbImpl implements GradeIO {
 
 	/* Put together the URL to get grades */
 
-	// Ensure the assignment title is ok and then really put the url together
+
 	public String formatGetURLTemplate(HashMap<String, String> values) throws GradeIOException {
+		// Ensure the assignment title is ok and then really put the url together
 		try {
 			values.put("ASSIGNMENTTITLE",
 					URLEncoder.encode(values.get("ASSIGNMENTTITLE"),"UTF-8").replaceAll("\\+", "%20"));
@@ -165,7 +214,7 @@ public class SPEEsbImpl implements GradeIO {
 		String url = formatPutURLTemplate(values);
 		M_log.debug("putGrades: request url: [" + url.toString() + "]");
 
-		WAPI wapi = new WAPI(values);
+		this.wapi = assureWAPI(this.wapi,speproperties.getIo());
 		WAPIResultWrapper wrappedResult = null;
 
 		wrappedResult = wapi.doPutRequest(url.toString(), headers);
@@ -177,13 +226,13 @@ public class SPEEsbImpl implements GradeIO {
 
 	// Template is set as an external io property.
 	public String formatPutURLTemplate(HashMap<String, String> values) {;
-		return String.format(
-				values.get("esbPutScoreTemplate"),
-				new Object[]{
-						values.get("apiPrefix"),
-						values.get("UNIQNAME"),
-						values.get("SCORE")
-				});
+	return String.format(
+			values.get("esbPutScoreTemplate"),
+			new Object[]{
+					values.get("apiPrefix"),
+					values.get("UNIQNAME"),
+					values.get("SCORE")
+			});
 	}
 
 	/************************** Check that can access ESB successfully *********************/
@@ -193,7 +242,8 @@ public class SPEEsbImpl implements GradeIO {
 	@Override
 	public boolean verifyConnection(SPEProperties speproperties) {
 
-		WAPI wapi = new WAPI(speproperties.getIo());
+		this.wapi = assureWAPI(this.wapi,speproperties.getIo());
+
 		WAPIResultWrapper tokenRenewal = wapi.renewToken();
 		Boolean success = false;
 
--- src/test/java/edu/umich/its/spe/SPEEsbTest.java
+++ src/test/java/edu/umich/its/spe/SPEEsbTest.java
@@ -7,9 +7,14 @@ package edu.umich.its.spe;
  */
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import java.io.IOException;
+import java.time.Instant;
 import java.util.HashMap;
 
 import org.apache.http.HttpStatus;
@@ -25,24 +30,29 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
+
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import edu.umich.ctools.esb.utils.WAPIResultWrapper;
+import edu.umich.ctools.esb.utils.WAPI;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest
 @ContextConfiguration
-@ComponentScan
 
 public class SPEEsbTest {
 
 	// Apply a global timeout to all tests.  Comment out when debugging a test.
-	@Rule
-	public Timeout globalTimeout = Timeout.seconds(10);
+	//	@Rule
+	//	public Timeout globalTimeout = Timeout.seconds(10);
+
+	@MockBean
+	PersistBlob persistblob;
+
+	static {
+		System.setProperty("skipRun", "TRUE");
+	}
 
 	@Before
 	public void setUp() throws Exception {
@@ -55,10 +65,13 @@ public class SPEEsbTest {
 
 	private GradeIO speesb;
 
-	@Autowired
+	// configure by hand (since there are nested values).
 	private SPEProperties speproperties;
 
-	@Autowired
+	@MockBean
+	private PersistTimestamp persisttimestamp;
+
+	@MockBean
 	private SPESummary spesummary;
 
 	private static Logger M_log = LoggerFactory.getLogger(SPEEsbTest.class);
@@ -66,27 +79,77 @@ public class SPEEsbTest {
 	String aprilFirst = "2017-04-01 18:00:00";
 
 	// First call fails so make it easy to run test twice.
-	protected void getGradesCommonMethod() throws GradeIOException {
+	protected void getGradesCommonMethod() throws GradeIOException, PersistBlobException {
+
+		when(persisttimestamp.readTestLastTakenTime()).thenReturn(Instant.now());
+		when(persisttimestamp.ensureLastTestTakenTime()).thenReturn(Instant.now());
+
+		// setup the mock properties
+		SPEProperties speproperties = setupMockIOProperties();
+
+		HashMap<String,String> getgrades = new HashMap<String,String>();
+		getgrades.put("ASSIGNMENTTITLE","HOWDY");
+		getgrades.put("COURSEID","187539");
+
+		speproperties.setGetgrades(getgrades);
+
+		// setup mock WAPI
+		WAPI wapi = mock(WAPI.class);
+		WAPIResultWrapper wrw = mock(WAPIResultWrapper.class);
+
+		// deal with token renewal
+		when(wapi.renewToken()).thenReturn(wrw);
+		when(wrw.getStatus()).thenReturn(HttpStatus.SC_OK);
+
+		// deal with request to wapi.
+		when(wapi.doRequest(any(),any())).thenReturn(wrw);
+		when(wrw.getStatus()).thenReturn(HttpStatus.SC_OK);
+
+		SPEEsbImpl speesb = new SPEEsbImpl(wapi,spesummary);
+
+		// now do something
 		WAPIResultWrapper wrappedResult = speesb.getGradesVia(speproperties,aprilFirst);
+
+		// check the results.
 		M_log.debug("grades: {}",wrappedResult.toJson());
 		assertNotNull("non-null result",wrappedResult);
 		Boolean callOk = wrappedResult.getStatus() == HttpStatus.SC_OK || wrappedResult.getStatus() == HttpStatus.SC_NOT_FOUND;
 		assertEquals("successful call",callOk, true);
 	}
 
+	public SPEProperties setupMockIOProperties() {
+		SPEProperties speproperties = new SPEProperties();
+
+		HashMap<String,String> tio = new HashMap<String,String>();
+		tio.put("COURSEID", "Kangaroo");
+		tio.put("esbGetScoreTemplate","%s/Scores/CourseId/%s/AssignmentTitle/%s");
+		tio.put("esbPutScoreTemplate","%s/UniqName/%s/Score/%s");
+		tio.put("key","OPENKEY");
+		tio.put("secret","SECRETTHINGEE");
+		tio.put("renewal","make me new again");
+		tio.put("apiPrefix","https://apigw-tst.it.umich.edu/um/aa/Unizin");
+		tio.put("tokenServer","https://apigw-tst.it.umich.edu/um/aa/oauth2/token");
+		tio.put("x-ibm-client-id","ksldfjsldkj");
+		tio.put("grant_type","hugh");
+		tio.put("scope","spanishplacementscores");
+
+		speproperties.setIo(tio);
+		return speproperties;
+	}
+
 	@Test
-	public void getGradesTest() throws IOException, GradeIOException {
+	public void getGradesTest() throws IOException, GradeIOException, PersistBlobException {
 		getGradesCommonMethod();
 	}
 
 	@Test
-	public void getGradesTest2() throws IOException, GradeIOException {
+	public void getGradesTest2() throws IOException, GradeIOException, PersistBlobException {
 		getGradesCommonMethod();
 	}
 
+	// put grades unit test not implemented
 	@Ignore
 	@Test
-	// fails SPEEsbTest.java
 	public void putGradesTest() throws IOException {
 
 		HashMap<String, String> user = new HashMap<String,String>();
@@ -103,9 +166,77 @@ public class SPEEsbTest {
 	@Test
 	public void checkVerifyTest() throws IOException {
 
+		SPEProperties speproperties = setupMockIOProperties();
+
+		// mock the wapi calls.
+		WAPI wapi = mock(WAPI.class);
+		WAPIResultWrapper wrw = mock(WAPIResultWrapper.class);
+
+		when(wapi.renewToken()).thenReturn(wrw);
+		when(wrw.getStatus()).thenReturn(HttpStatus.SC_OK);
+
+		SPEEsbImpl speesb = new SPEEsbImpl(wapi);
+
+		// now do something
 		Boolean verify_result = speesb.verifyConnection(speproperties);
 		M_log.debug("update: {}",verify_result);
 		assertTrue("successful verify",verify_result);
+	}
+
+
+	@Test
+	public void testAssureWAPICreatesWAPI() {
+
+		WAPI localWAPI = null;
+		SPEProperties speproperties = setupMockIOProperties();
+
+		SPEEsbImpl speesbimpl = new SPEEsbImpl(spesummary);
+
+		localWAPI = speesbimpl.assureWAPI(localWAPI,speproperties.getIo());
+		assertNotNull("create new wapi if current is null.",localWAPI);
+	}
+
+
+	@Test
+	public void testAssureWAPIRecyclesWAPIByDefault() {
+
+		WAPI localWAPI1 = null;
+		SPEProperties speproperties = setupMockIOProperties();
+
+		// Will test the impl.
+		SPEEsbImpl speesbimpl = new SPEEsbImpl(spesummary);
+
+		// make a wapi from nothing
+		localWAPI1 = speesbimpl.assureWAPI(localWAPI1,speproperties.getIo());
+		assertNotNull("create new wapi if current is null.",localWAPI1);
+
+		// get the same wapi back if ask again.
+		WAPI localWAPI2 = speesbimpl.assureWAPI(localWAPI1,speproperties.getIo());
+		assertNotNull("return a wapi even if already set.",localWAPI2);
+
+		assertEquals("assureWAPI returns same wapi multiple times.",localWAPI1,localWAPI2);
+	}
+
+
+	@Test
+	public void testAssureWAPINeedNotRecycleWAPI() {
+
+		WAPI localWAPI1 = null;
+		SPEProperties speproperties = setupMockIOProperties();
+
+		// need to test the implementation not the interface.
+
+		SPEEsbImpl speesbimpl = new SPEEsbImpl(false);
+
+		// make a wapi from nothing
+		localWAPI1 = speesbimpl.assureWAPI(localWAPI1,speproperties.getIo());
+		assertNotNull("create new wapi if current is null.",localWAPI1);
+
+		// ask for another one
+		WAPI localWAPI2 = speesbimpl.assureWAPI(localWAPI1,speproperties.getIo());
+		assertNotNull("return a wapi even if already set.",localWAPI2);
+
+		assertNotEquals("assureWAPI returns same wapi multiple times if requested.",localWAPI1,localWAPI2);
 	}
 
 }
